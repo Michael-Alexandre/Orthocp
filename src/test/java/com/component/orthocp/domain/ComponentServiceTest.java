202bfed5bb45c1a48d275b2107e3194319511611
Michael <michael.alexandre27@gmail.com>
2024-12-27 23:16:21 -0300

    Inject Mocks

--- src/test/java/com/component/orthocp/domain/ComponentServiceTest.java
+++ src/test/java/com/component/orthocp/domain/ComponentServiceTest.java
@@ -5,25 +5,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest(classes = ComponentService.class)
+@ExtendWith(MockitoExtension.class)
+// menos eficiente @SpringBootTest(classes = ComponentService.class)
 public class ComponentServiceTest {
-    @Autowired
+    
+    // spring @Autowired
+    @InjectMocks
     private ComponentService componentService;
 
-    @MockitoBean
+    // @MockitoBean
+    @Mock
     private ComponentRepository componentRepository;
 
     //nomear testes = operacao_estado_retorno
     @Test
     public void createComponent_valid_returnsComponent() {
+        // AAA = Arrange, act, assert
         when(componentRepository.save(COMPONENT)).thenReturn(COMPONENT);
-        // system under test
+        // sut = system under test; A = act
         Component sut = componentService.create(COMPONENT);
+        // a = assert
         assertThat(sut).isEqualTo(COMPONENT);
         
     }
